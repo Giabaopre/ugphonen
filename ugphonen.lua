462353186dadd1c87186d6422d4e4f2ce89f1fd6
Giabaopre <hotenla001@gmail.com>
2025-06-06 18:38:36 +0700

    Add files via upload

---
+++ ugphonen.lua
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         UGPhone Login Tool
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Ugphone Login
+// @author       Gia Bảo
+// @require      https://github.com/KingNotNgu/localstorage/raw/refs/heads/main/ugphone.js
+// @match        https://www.ugphone.com/*
+// @match        https://*.ugphone.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-end
+// @icon         https://image.kingdev.sbs/image/king.png?s=48&v=4
+// ==/UserScript==
